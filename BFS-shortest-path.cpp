1efed795527f582986045cb78d8b1aaaf8e73827
lonercoder101 <siddiquifaraz9@gmail.com>
2021-07-20 14:57:02 +0530

    breadth-first-search

--- BFS-shortest-path.cpp
+++ BFS-shortest-path.cpp
@@ -15,13 +15,21 @@ public:
 			l[j].push_back(i);
 	}
 
-	void bfs(int source){
+	void bfs(int source, int destination){
 
 		queue<int> q;
 		bool *visited = new bool [V]{false};
+		int *dist = new int[V]{0};
+		int *parent = new int[V];
+
+		for(int i=0;i<V;i++){
+			parent[i] = -1 ; 
+		} 
 
 		q.push(source);
 		visited[source] = true;
+		parent[source] = source;
+		dist[source] = 0;
 
 		while(!q.empty()){
 			// for every node do something
@@ -32,18 +40,49 @@ public:
 			for(auto nbr:l[f]){
 				if(!visited[nbr]){
 				q.push(nbr);
+				//parent and distance update
+				parent[nbr] = f;
+				dist[nbr] = dist[f] + 1;
 				visited[nbr] = true; 
 			}
 		}
 
 
 		}
+		//single source shortest path for undirected graph
+
+		// print path from source to any node which is shortest
+		// print the shortest distance
+
+		for(int i = 0 ; i< V; i++){
+			cout<<"shortest distance to "<< i<< " is "<< dist[i] << '\n';
+		}
+		cout<<endl;
+
+		if(destination!=-1){
+
+			int temp = destination;
+			while(temp!=source){
+
+				cout<<temp<<"-->";
+
+				temp = parent[temp];
+
+
+
+			}
+			cout<<source<<'\n';
+
+
+
 
 	}
-
-
-
+}
 };
+
+
+
+
 int main()
 {
 	Graph g(7);
@@ -55,6 +94,7 @@ int main()
 	g.addEdge(4,5);
 	g.addEdge(0,4);
 	g.addEdge(3,4);
-	g.bfs(1);
+	g.bfs(1,6);
+
 	return 0;
 }
