538340b4ab7a75069dc6548b208d9927d8d3d39f
lonercoder101 <siddiquifaraz9@gmail.com>
2021-08-20 16:52:30 +0530

    Create DSU_NPTEL.cpp

---
+++ DSU_NPTEL.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class UnionFind {
+	private:
+		vector<int> p,rank,setSize;
+		int numSets;
+
+	public:
+		UnionFind(int N){
+			p.assign(N,0);
+
+			for(int i = 0; i < N; i++)
+				p[i] = i;
+
+			rank.assign(N,0);
+			setSize.assign(N,1);
+			numSets = N;
+		}
+
+		int findSet(int i){
+			if(p[i]==i) return i;
+			else return p[i] = findSet(p[i]);
+		}
+
+		bool isSameSet(int i, int j){
+			return findSet(i) == findSet(j);
+		}
+
+		int sizeOfSet(int i){
+			return setSize[findSet(i)];
+		}
+		int numDisjointSets(){
+			return numSets;
+		}
+		void unionSet(int i, int j){
+			if (isSameSet(i,j))
+				return;
+			int x = findSet(i);
+			int y = findSet(j);
+
+			if( rank[x] > rank[y] )
+				swap(x,y);
+			p[x]=y;
+
+			if( rank[x] == rank[y] )
+				rank[y]++;
+
+			setSize[y] += setSize[x];
+			numSets--;
+		}
+
+};
+int x,y;
+int main(){
+
+
+	int n,m;
+	cin >> n >> m;
+	UnionFind obj(n+1);
+
+
+}
